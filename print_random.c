93488775f7a95be577000f0b057cfd527119437f
Lydon Gavin <lydon.gavin@temple.edu>
2024-09-01 13:25:23 -0400

    updated print_random.c

--- print_random.c
+++ print_random.c
@@ -4,22 +4,17 @@
 #include <string.h>
 #include "random.h"
 
-void rand_string(char *s, size_t length) {
-    for (size_t i = 0; i < length; i++) { 
-        s[i] = randchar();
-    }
-}
-char* rand_string_alloc(size_t size)
-{
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
-}
-
 int main() {
-    srand(time(NULL));
-    printf("%s\n", rand_string_alloc(7));
-    return 0;
+   
+    int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
--- random.h
+++ random.h
@@ -4,5 +4,4 @@
 #include <string.h>
 
 char randchar(void);
-void rand_string(char *s, size_t length);
 char* rand_string_alloc(size_t size);
